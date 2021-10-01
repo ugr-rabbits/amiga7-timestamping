ad68ee12a44fc0ce593eb292a99a1358facb485d
Víctor Vázquez <victorvazrod@ugr.es>
2021-10-01 14:38:15 +0200

    feat: add document signing

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/ugr-rabbits/amiga7-timestamping
 
 go 1.17
 
-require github.com/gin-gonic/gin v1.7.4
+require (
+	github.com/digitorus/timestamp v0.0.0-20210102082646-54ddd7720e27
+	github.com/gin-gonic/gin v1.7.4
+)
 
 require (
+	github.com/digitorus/pkcs7 v0.0.0-20200320092839-808436b6f6d1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 
+	ts "github.com/digitorus/timestamp"
 	"github.com/gin-gonic/gin"
 )
 
@@ -42,8 +44,29 @@ func main() {
 
 	// Sign a document
 	r.POST("/sign", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"msg": "Vale crack",
+		file, header, err := c.Request.FormFile("doc")
+		if err != nil {
+			panic(err)
+		}
+
+		tsReq, err := ts.CreateRequest(file, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		req, err := http.NewRequest(http.MethodGet, TSA_SERVICE, bytes.NewReader(tsReq))
+		if err != nil {
+			panic(err)
+		}
+		req.Header.Set("Content-Type", "application/timestamp-query")
+
+		tsResp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			panic(err)
+		}
+
+		c.DataFromReader(http.StatusOK, tsResp.ContentLength, tsResp.Header.Get("Content-Type"), tsResp.Body, map[string]string{
+			"Content-Disposition": "attachment; filename=\"" + header.Filename + ".tsr\"",
 		})
 	})
 
