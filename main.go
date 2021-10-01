31a47ecccb9458a8c485556ae431e9a5e612a34d
Víctor Vázquez <victorvazrod@ugr.es>
2021-10-01 13:00:27 +0200

    feat: add proxy requests for certificate downloads

--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
 
+const TSA_SERVICE = "http://localhost:2020"
+
 func main() {
+	remote, err := url.Parse(TSA_SERVICE)
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 	r.LoadHTMLGlob("web/templates/*")
 	r.Static("/assets", "web/static")
 
+	// Serve index page
 	r.GET("/", func(c *gin.Context) {
 		if c.GetHeader("Content-Type") == "application/timestamp-query" {
 			fmt.Println("Timestamp request")
@@ -19,6 +29,33 @@ func main() {
 		c.HTML(http.StatusOK, "index.go.html", gin.H{})
 	})
 
+	// Proxy download CA certificate
+	r.GET("/certs/ca", func(c *gin.Context) {
+		proxy := httputil.NewSingleHostReverseProxy(remote)
+		proxy.Director = func(req *http.Request) {
+			req.Header = c.Request.Header
+			req.Host = remote.Host
+			req.URL.Scheme = remote.Scheme
+			req.URL.Host = remote.Host
+			req.URL.Path = "/ca.pem"
+		}
+		proxy.ServeHTTP(c.Writer, c.Request)
+	})
+
+	// Proxy download TSA certificate
+	r.GET("/certs/tsa", func(c *gin.Context) {
+		proxy := httputil.NewSingleHostReverseProxy(remote)
+		proxy.Director = func(req *http.Request) {
+			req.Header = c.Request.Header
+			req.Host = remote.Host
+			req.URL.Scheme = remote.Scheme
+			req.URL.Host = remote.Host
+			req.URL.Path = "/tsa_cert.pem"
+		}
+		proxy.ServeHTTP(c.Writer, c.Request)
+	})
+
+	// Proxy sign a document
 	r.POST("/sign", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "Vale crack",
